4c27812b89d48052927f65e3b04bc4c8b145a22e
Andreas Voegele <andreas@andreasvoegele.com>
2021-01-13 17:40:13 +0100

    Build centos7 for libvirt

--- centos7/scripts/cleanup.sh
+++ centos7/scripts/cleanup.sh
@@ -4,8 +4,11 @@
 yum -y remove ansible
 
 # Zero out the rest of the free space using dd, then delete the written file.
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
+#dd if=/dev/zero of=/EMPTY bs=1M
+#rm -f /EMPTY
+
+# Trim filesystems to free space.
+fstrim -av
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
 sync
